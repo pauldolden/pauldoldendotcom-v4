bdd22297ddbb4c8a670619daa1f856995d0e7416
Paul Dolden <pauldolden@Pauls-MBP.lan>
2022-06-27 17:41:42 +0100

    changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,6 @@
 	import '../app.css';
 </script>
 
-<svelte:body class="bg-palette-background" />
-
 <main
 	class="bg-palette-background text-palette-text min-h-screen w-screen flex items-center justify-center font-rubik font-semibold"
 >
