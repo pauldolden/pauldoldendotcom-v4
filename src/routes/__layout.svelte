8d6bf600e5afbeaec21a941c5ed8d6fb05e452a1
Paul Dolden <pauldolden@Pauls-MBP.lan>
2022-06-27 17:38:33 +0100

    changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 <svelte:body class="bg-palette-background" />
 
 <main
-	class="bg-palette-background text-palette-text h-screen w-screen flex items-center justify-center font-rubik font-semibold"
+	class="bg-palette-background text-palette-text min-h-screen w-screen flex items-center justify-center font-rubik font-semibold"
 >
 	<slot />
 </main>
